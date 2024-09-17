d6d9612c1c3887df5b11a8d51cd976ecceb8fc45
Praveen-Thathsara <praveenstudy823@gmail.com>
2024-09-17 10:30:31 +0530

    add a pagination

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -15,10 +15,32 @@ const Gallery = () => {
   ];
 
   return (
+    <div calssName="row">
     <div className="gallery">
       {images.map((image, index) => (
         <ImageCard key={index} image={image} />
       ))}
+
+      </div>
+      <div calssName="row">
+      <nav aria-label="Page navigation example">
+  <ul className="pagination justify-content-center">
+    <li className="page-item">
+      <a className="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li className="page-item"><a className="page-link" href="#">1</a></li>
+    <li className="page-item"><a className="page-link" href="#">2</a></li>
+    <li className="page-item"><a className="page-link" href="#">3</a></li>
+    <li className="page-item">
+      <a className="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+    </div>
     </div>
   );
 };
